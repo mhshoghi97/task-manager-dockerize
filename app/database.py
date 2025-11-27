73ab37d9b0ad09a98889e59189695634d1c5cba2
mhshoghi97 <mhshoghi97@gmail.com>
2025-11-27 15:14:26 +0330

    Refactor DeclarativeBase to declarative_base - changed to Base = declarative_base()
    This will be solved in docker coompose build

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import DeclarativeBase
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
@@ -7,7 +7,7 @@ from app.core.config import settings
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
-Base = DeclarativeBase()
+Base = declarative_base()
 
 
 def get_db():
